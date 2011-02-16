4fd5a48d324e6063197f43a59d4b982a6a0c96b7
nowotny <nowotnypl@gmail.com>
2011-02-16 17:41:36 +0000

    Adding polish translation for the plugin

---
+++ i18n/pl-message.php
@@ -0,0 +1,71 @@
+<?php
+
+    /**
+     * Polish file for plugin mbforms
+     *
+     * @package Plugins
+     * @subpackage mbforms
+     *
+     * @author Piotr Łukaszewski <nowotnypl@gmail.pl>
+     * @version Wolf 0.7.3
+     */
+
+    return array(   	
+	'Add Item' => 'Dodaj element',
+	'Are you sure?' => 'Czy jesteś pewien?',
+	'Checkbox' => 'Pole wielokrotnego wyboru',
+	'Create Form' => 'Utwórz formularz',
+	'Delete' => 'Usuń',
+	'Delete Item' => 'Usuń element',
+	'Denotes required fields' => 'Oznacza pola wymagane',
+	'Displaying Your Forms' => 'Wyświetlanie formularzy',
+	'Drop Down' => 'Rozwijane pole wyboru',
+	'Edit' => 'Edytuj',
+	'Edit Form' => 'Edytuj formularz',
+	'Email Results To' => 'Wyślij wyniki do',
+	'Extras' => 'Atrybuty',
+	'Failed to delete the form!' => 'Nie udało się usunąć formularza!',
+	'Failed to save your form, please try again.' => 'Nie udało się zapisać formularza, spróbuj ponownie.',
+	'Failed to send your email! Please try again' => 'Nie udało się wysłać emaila! Spróbuj ponownie.',
+	'For classes, JS events etc...' => 'Do określania klas, zdarzeń Javaskrypt, itd...',
+	'For drop downs and radio buttons. Seperate the options with a comma.' => 'Dla rozwijanych pól wyboru i pól jednokrotnego wyboru',
+	'Form Builder' => 'Formularze',
+	'Form List' => 'Lista formularzy',
+	'Form Name' => 'Nazwa formularza',
+	'Form Settings' => 'Ustawienia formularzy',
+	'Form has been deleted!' => 'Formularz został usunięty!',
+	'Form has been saved!' => 'Formularz został zapisany.',
+	'Form has been saved, however <span style="text-decoration: underline">none</span> of the form fields have been saved.' => 'Formularz został zapisany, jednakże <span style="text-decoration: underline">żadne</span> z pól formularza nie zostało zapisane.',
+	'Form has been saved, however not all the form items have been saved.' => 'Formularz został zapisany, jednakże nie wszystkie pola formularza zostały zapisane.',
+	'Forms' => 'Formularze',
+	'Input Box' => 'Pole wprowadzania tekstu',
+	'Items' => 'Elementy',
+	'Label' => 'Etykieta',
+	'Order' => 'Kolejność',
+	'Please Select an Option' => 'Wybierz opcję',
+	'Radio Buttons' => 'Pole jednokrotnego wyboru',
+	'Required?' => 'Wymagane?',
+	'Save Form' => 'Zapisz',
+	'Save Settings' => 'Zapisz',
+	'Select Template' => 'Wybierz wzorzec',
+	'Settings have been saved!' => 'Ustawienia nie zostały zapisane',
+	'Submission' => 'Wysyłanie',
+	'Submit Form' => 'Wyślij',
+	'Success Template' => 'Wzorzec pozytywnego rezultatu',
+	'Text Area' => 'Duże pole tekstowe',
+	'There has been an email submission via :name. See below for submission details' => '',
+	'There were one or more errors with the data you entered!' => 'Wystąpiły błędy! Dane niepoprawne!',
+	'Type' => 'Typ',
+	'Use Captcha' => 'Użyj Captcha',
+	'Values' => 'Wartości',
+	'You did not enter the correct captcha code, please try again!' => 'Kod Captcha niepoprawny spróbuj ponownie.',
+	'You do not have permission to access the requested page!' => 'Nie masz uprawnień do tej strony!',
+	'You have not filled out ":label"' => 'Nie wypełniono pola ":label"',
+	'You must add at least one form item.' => 'Musisz dodać przynajmniej jeden element formularza.',
+	'You must fill out a name for this form' => 'Musisz podać nazwę formularza',
+	'You must fill out a recipient email address for the form.' => 'Musisz podać adres email odbiorców formularza.',
+	'You must select a success template.' => 'Musisz wybrać wzorzec pozytywnego rezultatu',
+	'You must select at least one item for the form.' => 'Musisz wybrać przynajmniej jeden element formularza.',
+	'reCaptcha Private Key' => 'Klucz prywatny reCaptcha',
+	'reCaptcha Public Key' => 'Klucz publiczny reCaptcha',
+    );
